3c151bdda848e045c8ef26275e0acf2a5116be51
Anex <asnekhaev@gmail.com>
2026-05-06 14:06:27 +0300

    release: v0.8.0-tunnel-import

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -19,7 +19,7 @@ import (
 )
 
 // Version is overridable at link time: -ldflags "-X main.Version=0.5.0"
-var Version = "0.5.0-awgmgr-pivot"
+var Version = "0.8.0-tunnel-import"
 
 func main() {
 	configPath := flag.String("config", "/opt/etc/wg-monitor/config.yaml", "path to YAML config")
